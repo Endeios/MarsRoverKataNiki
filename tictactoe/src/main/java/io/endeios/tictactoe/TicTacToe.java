f4bc65485db66b2dd568b27fd2c7ccdf004010fa
Bruno Mario Veronesi <endeios@gmail.com>
2023-03-05 23:33:25 +0100

    adding player switch

--- tictactoe/src/main/java/io/endeios/tictactoe/TicTacToe.java
+++ tictactoe/src/main/java/io/endeios/tictactoe/TicTacToe.java
@@ -6,21 +6,35 @@ import org.slf4j.LoggerFactory;
 
 public class TicTacToe {
 
+    Player currentPlayer;
+
     private static final Logger logger = LoggerFactory.getLogger(TicTacToe.class);
 
     public static void main(String[] args) {
         logger.info("Welcome to Tick tack toe");
     }
 
+    public TicTacToe() {
+        this.currentPlayer = Player.X;
+    }
+
     public Player currentPlayer() {
-        return Player.X;
+        return currentPlayer;
     }
 
     public void play(int column, int row) {
-
+        switchPlayer();
     }
 
-    public Player getStatusForPosition(int i, int i1) {
+    private void switchPlayer() {
+        if (currentPlayer == Player.X) {
+            currentPlayer = Player.O;
+        } else {
+            currentPlayer = Player.X;
+        }
+    }
+
+    public Player getStatusForPosition(int column, int row) {
         return Player.X;
     }
 }
--- tictactoe/src/test/java/io/endeios/tictactoe/TicTacToeTest.java
+++ tictactoe/src/test/java/io/endeios/tictactoe/TicTacToeTest.java
@@ -1,27 +1,38 @@
 package io.endeios.tictactoe;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TicTacToeTest {
+    private TicTacToe ticTacToe;
+
+    @BeforeEach
+    void setUp() {
+        ticTacToe = new TicTacToe();
+    }
+
     @Test
     void canBeCreated() {
-        TicTacToe ticTacToe = new TicTacToe();
         assertNotNull(ticTacToe);
     }
 
     @Test
     void playerXIsTheFirstToStart() {
-        TicTacToe ticTacToe = new TicTacToe();
         assertSame(Player.X, ticTacToe.currentPlayer());
     }
 
     @Test
     void playerCanPlayAPosition() {
-        TicTacToe ticTacToe = new TicTacToe();
         ticTacToe.play(0, 0);
         assertEquals(Player.X, ticTacToe.getStatusForPosition(0, 0));
+    }
+
+    @Test
+    void playerOPlaysAfterX() {
+        ticTacToe.play(0, 0);
+        assertSame(Player.O, ticTacToe.currentPlayer());
     }
 }
